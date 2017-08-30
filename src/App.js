133d4cd7f2cae541e056f9a60b876470cc5a5e8a
Whaledevelop <whaledevelop@gmail.com>
2017-08-30 09:08:37 +0300

    Don't understand what happening

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends React.Component {
           lastMonthFilter: newLastMonthFilter,
           moreThanThousandRubFilter: newMoreThanThousandRubFilter
       });
+
   }
 
   render () {
