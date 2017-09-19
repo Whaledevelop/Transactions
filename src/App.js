5d12b16484d65bf9c13512ed8a681e4948c76ba3
Whaledevelop <whaledevelop@gmail.com>
2017-09-19 22:16:50 +0300

    Small refactoring

--- src/App.js
+++ src/App.js
@@ -18,54 +18,57 @@ class App extends Component {
     this.handleFilters = this.handleFilters.bind(this);
   }
 
-  handleFilters(newIncomeFilter, newConsumptionFilter, newLastMonthFilter, newMoreThanThousandRubFilter) {
+  handleFilters(newIncomeFilter, newConsumptionFilter,
+     newLastMonthFilter, newMoreThanThousandRubFilter) {
       this.setState({
           incomeFilter: newIncomeFilter,
           consumptionFilter: newConsumptionFilter,
           lastMonthFilter: newLastMonthFilter,
           moreThanThousandRubFilter: newMoreThanThousandRubFilter
       });
-
   }
-
-  
 
   render () {
     const filterTransactions = () => {
         let filteredTransactions =  transactionsList;
-        if (this.state.incomeFilter || this.state.consumptionFilter || this.state.lastMonthFilter || this.state.moreThanThousandRubFilter) {
+        if (this.state.incomeFilter || this.state.consumptionFilter || 
+          this.state.lastMonthFilter || this.state.moreThanThousandRubFilter) {
             if (this.state.incomeFilter) {
-              filteredTransactions = filteredTransactions.filter( t => t.type === 'income');
+              filteredTransactions = filteredTransactions.filter(transaction =>
+                 transaction.type === 'income');
             }
             if (this.state.consumptionFilter) {
-              filteredTransactions = filteredTransactions.filter (t => t.type === 'consumption');
+              filteredTransactions = filteredTransactions.filter (transaction =>
+                 transaction.type === 'consumption');
             }
             if (this.state.lastMonthFilter) {
               const monthAgo = moment().subtract(30, 'days').calendar();
               const formattedMonthAgo = moment(monthAgo).format('YYMMDD');
-              filteredTransactions = filteredTransactions.filter (t => moment(t.date).format('YYMMDD') > formattedMonthAgo);
+              filteredTransactions = filteredTransactions.filter(transaction => 
+                moment(transaction.date).format('YYMMDD') > formattedMonthAgo);
             }
             if (this.state.moreThanThousandRubFilter) {
-              filteredTransactions = filteredTransactions.filter (t => t.value > 1000);
+              filteredTransactions = filteredTransactions.filter (transaction => 
+                transaction.value > 1000);
             }
         }
         return filteredTransactions.map(transaction => {
-                        return <TransactionsTable transaction={transaction} key={transaction.id}/>
-                      })
+                        return <TransactionsTable 
+                                  transaction={transaction} 
+                                  key={transaction.id}/>
+        })
     };
-  
 
    return (
       <div className="container-fluid">
           <div className="col-md-12 col-lg-12">
               <button type='button' className="btn btn-default" id="addButton">Добавить транзакцию</button>
               <Filters 
-                onClick={this.handleFilters}
-                incomeFilter={this.state.incomeFilter}
-                consumptionFilter={this.state.consumptionFilter}
-                lastMonthFilter={this.state.lastMonthFilter}
-                moreThanThousandRubFilter={this.state.moreThanThousandRubFilter}
-                />
+                  onClick={this.handleFilters}
+                  incomeFilter={this.state.incomeFilter}
+                  consumptionFilter={this.state.consumptionFilter}
+                  lastMonthFilter={this.state.lastMonthFilter}
+                  moreThanThousandRubFilter={this.state.moreThanThousandRubFilter}/>
               <table className="table table-striped table-hover">
                 <thead>
                     <tr>
@@ -77,11 +80,10 @@ class App extends Component {
                 </thead>
                 <tbody>
                   {filterTransactions(
-                    transactionsList, 
-                    this.state.incomeFilter, 
-                    this.state.consumptionFilter, 
-                    this.state.lastMonthFilter, 
-                    this.state.moreThanThousandRubFilter
+                      this.state.incomeFilter, 
+                      this.state.consumptionFilter, 
+                      this.state.lastMonthFilter, 
+                      this.state.moreThanThousandRubFilter
                     )
                 }
                 </tbody>
