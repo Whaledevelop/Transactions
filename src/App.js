67e11fbe9af6c06ad046ae08ed305683e7665459
Whaledevelop <whaledevelop@gmail.com>
2017-09-21 13:02:51 +0300

    small functionality here in redux

--- src/App.js
+++ src/App.js
@@ -1,145 +1,24 @@
 import React, {Component} from 'react';
-import './bootstrap.css'
-import './App.css';
-<<<<<<< HEAD
-import {TransactionsTable} from './TransactionsTable';
-import {transactionsList} from './transactionsList';
-import {Filters} from './Filters';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
 
-class App extends Component {
-  constructor(props) {
-  super(props);
-  this.state = {
-                  incomeFilter: false,
-                  consumptionFilter: false,
-                  lastMonthFilter: false,
-                  moreThanThousandRubFilter: false
-    }
-    this.handleFilters = this.handleFilters.bind(this);
-  }
 
-  handleFilters(newIncomeFilter, newConsumptionFilter,
-     newLastMonthFilter, newMoreThanThousandRubFilter) {
-=======
+import './bootstrap.css'
+import './App.css';
 
 import TransactionsTable from './TransactionsTable';
-import transactionsList from './transactionsList';
-import Filters from './Filters';
-
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-                    incomeFilter: false,
-                    consumptionFilter: false,
-                    lastMonthFilter: false,
-                    moreThanFilter: false
-      }
-      this.handleFilters = this.handleFilters.bind(this);
-  }
-
-  handleFilters(newIncomeFilter, newConsumptionFilter,
-     newLastMonthFilter, newMoreThanFilter) {
-<<<<<<< HEAD
->>>>>>> parent of fcc0987... Added add form
-=======
->>>>>>> parent of fcc0987... Added add form
-      this.setState({
-          incomeFilter: newIncomeFilter,
-          consumptionFilter: newConsumptionFilter,
-          lastMonthFilter: newLastMonthFilter,
-<<<<<<< HEAD
-<<<<<<< HEAD
-          moreThanThousandRubFilter: newMoreThanThousandRubFilter
-      });
-=======
-=======
->>>>>>> parent of fcc0987... Added add form
-          moreThanFilter: newMoreThanFilter
-      });
-  }
-
   addTransaction() {
     this.props.onAddTransaction(this.transactionInput.value, this.props.transactions);
     this.transactionInput.value = '';
-<<<<<<< HEAD
->>>>>>> parent of fcc0987... Added add form
-=======
->>>>>>> parent of fcc0987... Added add form
   }
 
   render () {
-    const filterTransactions = () => {
-        let filteredTransactions =  transactionsList;
-        if (this.state.incomeFilter || this.state.consumptionFilter || 
-<<<<<<< HEAD
-<<<<<<< HEAD
-          this.state.lastMonthFilter || this.state.moreThanThousandRubFilter) {
-=======
-          this.state.lastMonthFilter || this.state.moreThanFilter) {
->>>>>>> parent of fcc0987... Added add form
-=======
-          this.state.lastMonthFilter || this.state.moreThanFilter) {
->>>>>>> parent of fcc0987... Added add form
-            if (this.state.incomeFilter) {
-              filteredTransactions = filteredTransactions.filter(transaction =>
-                 transaction.type === 'income');
-            }
-            if (this.state.consumptionFilter) {
-              filteredTransactions = filteredTransactions.filter (transaction =>
-                 transaction.type === 'consumption');
-            }
-            if (this.state.lastMonthFilter) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-              const monthAgo = moment().subtract(30, 'days').calendar();
-              const formattedMonthAgo = moment(monthAgo).format('YYMMDD');
-              filteredTransactions = filteredTransactions.filter(transaction => 
-                moment(transaction.date).format('YYMMDD') > formattedMonthAgo);
-            }
-            if (this.state.moreThanThousandRubFilter) {
-=======
-=======
->>>>>>> parent of fcc0987... Added add form
-              const monthAgo = moment().subtract(30, 'days').format('YYMMDD');
-              filteredTransactions = filteredTransactions.filter(transaction => 
-                moment(transaction.date).format('YYMMDD') > monthAgo);
-            }
-            if (this.state.moreThanFilter) {
-<<<<<<< HEAD
->>>>>>> parent of fcc0987... Added add form
-=======
->>>>>>> parent of fcc0987... Added add form
-              filteredTransactions = filteredTransactions.filter (transaction => 
-                transaction.value > 1000);
-            }
-        }
-        return filteredTransactions.map(transaction => {
-                        return <TransactionsTable 
-                                  transaction={transaction} 
-                                  key={transaction.id}/>
-        })
-    };
-
    return (
       <div className="container">
           <div className="col-md-12 col-lg-12">
-<<<<<<< HEAD
-<<<<<<< HEAD
-              <button type='button' className="btn btn-default" id="addButton">Добавить транзакцию</button>
-              <Filters 
-                  onClick={this.handleFilters}
-                  incomeFilter={this.state.incomeFilter}
-                  consumptionFilter={this.state.consumptionFilter}
-                  lastMonthFilter={this.state.lastMonthFilter}
-                  moreThanThousandRubFilter={this.state.moreThanThousandRubFilter}/>
-              <table className="table table-striped table-hover">
-                <thead>
-=======
-=======
->>>>>>> parent of fcc0987... Added add form
               <div>
                   <Link to="/add">
                       <button type="button" className="btn btn-default" id="addButton">Add transaction</button>
@@ -170,49 +49,6 @@ class App extends Component {
                           )}
                   </tbody>
               </table>                          
-              <Filters 
-                  onClick={this.handleFilters}
-                  incomeFilter={this.state.incomeFilter}
-                  consumptionFilter={this.state.consumptionFilter}
-                  lastMonthFilter={this.state.lastMonthFilter}
-                  moreThanFilter={this.state.moreThanFilter}/>
-              <table className="table table-striped table-hover">
-                <thead>
-<<<<<<< HEAD
->>>>>>> parent of fcc0987... Added add form
-=======
->>>>>>> parent of fcc0987... Added add form
-                    <tr>
-                      <th>id</th>
-                      <th>Value</th>
-                      <th>Type</th>
-                      <th>Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                  {filterTransactions(
-                      this.state.incomeFilter, 
-                      this.state.consumptionFilter, 
-                      this.state.lastMonthFilter, 
-<<<<<<< HEAD
-<<<<<<< HEAD
-                      this.state.moreThanThousandRubFilter
-                    )
-                }
-                </tbody>
-              </table>              
-=======
-=======
->>>>>>> parent of fcc0987... Added add form
-                      this.state.moreThanFilter
-                    )
-                }
-                </tbody>
-              </table>            
-<<<<<<< HEAD
->>>>>>> parent of fcc0987... Added add form
-=======
->>>>>>> parent of fcc0987... Added add form
           </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { HashRouter, Route} from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import AddForm from './AddForm';
