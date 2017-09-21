a54837b2cc0dfbd54ab8832f300d5708c93efc9c
Whaledevelop <whaledevelop@gmail.com>
2017-09-21 12:24:30 +0300

    Revert "Added mindmap to the project"
    This reverts commit 4d9a65c21b6d367dec827f87d3329aa562470be3.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+<<<<<<< HEAD
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -20,9 +21,72 @@ class App extends Component {
   }
 
   render () {
+=======
+import './bootstrap.css'
+import './App.css';
+import {TransactionsTable} from './TransactionsTable';
+import {transactionsList} from './transactionsList';
+import {Filters} from './Filters';
+import moment from 'moment';
+
+class App extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+                  incomeFilter: false,
+                  consumptionFilter: false,
+                  lastMonthFilter: false,
+                  moreThanThousandRubFilter: false
+    }
+    this.handleFilters = this.handleFilters.bind(this);
+  }
+
+  handleFilters(newIncomeFilter, newConsumptionFilter,
+     newLastMonthFilter, newMoreThanThousandRubFilter) {
+      this.setState({
+          incomeFilter: newIncomeFilter,
+          consumptionFilter: newConsumptionFilter,
+          lastMonthFilter: newLastMonthFilter,
+          moreThanThousandRubFilter: newMoreThanThousandRubFilter
+      });
+  }
+
+  render () {
+    const filterTransactions = () => {
+        let filteredTransactions =  transactionsList;
+        if (this.state.incomeFilter || this.state.consumptionFilter || 
+          this.state.lastMonthFilter || this.state.moreThanThousandRubFilter) {
+            if (this.state.incomeFilter) {
+              filteredTransactions = filteredTransactions.filter(transaction =>
+                 transaction.type === 'income');
+            }
+            if (this.state.consumptionFilter) {
+              filteredTransactions = filteredTransactions.filter (transaction =>
+                 transaction.type === 'consumption');
+            }
+            if (this.state.lastMonthFilter) {
+              const monthAgo = moment().subtract(30, 'days').calendar();
+              const formattedMonthAgo = moment(monthAgo).format('YYMMDD');
+              filteredTransactions = filteredTransactions.filter(transaction => 
+                moment(transaction.date).format('YYMMDD') > formattedMonthAgo);
+            }
+            if (this.state.moreThanThousandRubFilter) {
+              filteredTransactions = filteredTransactions.filter (transaction => 
+                transaction.value > 1000);
+            }
+        }
+        return filteredTransactions.map(transaction => {
+                        return <TransactionsTable 
+                                  transaction={transaction} 
+                                  key={transaction.id}/>
+        })
+    };
+
+>>>>>>> parent of bdadb88... Another name refact
    return (
       <div className="container">
           <div className="col-md-12 col-lg-12">
+<<<<<<< HEAD
               <div className="col-lg-4">
                   <div>
                       <Link to="/add">
@@ -82,6 +146,34 @@ class App extends Component {
                           )}
                   </tbody>
                 </table>                       
+=======
+              <button type='button' className="btn btn-default" id="addButton">Добавить транзакцию</button>
+              <Filters 
+                  onClick={this.handleFilters}
+                  incomeFilter={this.state.incomeFilter}
+                  consumptionFilter={this.state.consumptionFilter}
+                  lastMonthFilter={this.state.lastMonthFilter}
+                  moreThanThousandRubFilter={this.state.moreThanThousandRubFilter}/>
+              <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                      <th>id</th>
+                      <th>Value</th>
+                      <th>Type</th>
+                      <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                  {filterTransactions(
+                      this.state.incomeFilter, 
+                      this.state.consumptionFilter, 
+                      this.state.lastMonthFilter, 
+                      this.state.moreThanThousandRubFilter
+                    )
+                }
+                </tbody>
+              </table>              
+>>>>>>> parent of bdadb88... Another name refact
           </div>
       </div>
     );
--- src/TransactionsTable.js
+++ src/TransactionsTable.js
@@ -7,7 +7,13 @@ export class TransactionsTable extends Component {
 		         <td>{this.props.transaction.id}</td>
 		         <td>{this.props.transaction.value}</td>
 		         <td>{this.props.transaction.type}</td> 
+<<<<<<< HEAD
 		         <td>{this.props.transaction.date}</td>
+=======
+		         <td>
+		         	{moment(this.props.transaction.date).format('HH:mm - DD.MM.YYYY')}
+		         </td>
+>>>>>>> parent of bdadb88... Another name refact
 		   </tr>
 	    )
   	}
