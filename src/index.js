382c0d0cb916fc6934f0f49c78bb119536242262
Whaledevelop <whaledevelop@gmail.com>
2017-09-22 14:41:57 +0300

    Redux in routing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { HashRouter, Route} from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 import AddForm from './AddForm';
+import reducer from './reducers';
+
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ 
+  && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
+    <Provider store={store}>
       <HashRouter>
         <div>
           <Route exact path="/" component={App}/>
           <Route path="/add" component={AddForm}/>
         </div>
-      </HashRouter>,
+      </HashRouter>
+    </Provider>,  
     document.getElementById('root')
   );
+
 
