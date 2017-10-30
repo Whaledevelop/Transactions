573783a645bae5d104c63e569c21ec28fde46c8b
Whaledevelop <whaledevelop@gmail.com>
2017-10-30 08:44:42 +0300

    More merging

--- src/containers/Transactions.jsx
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux'
-import { filtersHandler } from '../components/modules/filtersHandler'
-import TransactionsList from '../components/TransactionsList'
-
-const Transactions = connect(
-        state => ({ transactions: filtersHandler(state.transactions, state.visibilityFilters) }),
-)(TransactionsList)
-
-export default Transactions
